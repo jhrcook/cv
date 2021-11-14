8b7a8beeb80fd4631f510486dacb960cfff4674b
Josh Cook <joshuacook0023@gmail.com>
2021-11-13 19:25:59 -0500

    style: begin styling my CV; generate PDF

--- src/pagedown-helpers.R
+++ src/pagedown-helpers.R
@@ -2,6 +2,8 @@
 library(glue)
 library(tidyverse)
 
+# ---- General ----
+
 table_to_section <- function(data) {
 
   data <- data %>%
@@ -25,4 +27,58 @@ table_to_section <- function(data) {
 
   "
   print(glue_data(data, glue_template))
+}
+
+# ---- Publications ----
+
+combine_strings <- function(s) {
+  paste(s, collapse = ", ")
+}
+
+specific_title_changes <- function(s) {
+  s %>%
+    str_replace("\\$\\\\beta\\$", "β")
+}
+
+clean_strings <- function(s) {
+  unlist(s) %>%
+    str_squish() %>%
+    str_replace_all("\\{|\\}", "*")
+}
+
+highlight_me <- function(authors) {
+  ifelse(str_detect(authors, "Joshua"), glue("**{authors}**"), authors)
+}
+
+format_authors <- function(pub) {
+  as.character(pub$author) %>%
+    unlist() %>%
+    highlight_me() %>%
+    combine_strings() %>%
+    clean_strings()
+}
+
+print_pubs <- function(pub) {
+  glue_template <- "
+  ### {title}
+
+  *{journal}*
+
+  N/A
+
+  {year}
+
+  {authors}
+
+  "
+
+  data <- list(
+    title = clean_strings(pub$title) %>% specific_title_changes(),
+    journal = clean_strings(pub$journal),
+    year = pub$year,
+    authors = format_authors(pub)
+  )
+
+  print(glue_data(data, glue_template))
+  return(NULL)
 }
