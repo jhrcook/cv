2e31f88bfddcad29dc3976fff6bc6be189c901ff
Josh Cook <joshuacook0023@gmail.com>
2021-11-14 08:49:43 -0500

    feat: script to render CV

---
+++ render-cv.R
@@ -0,0 +1,32 @@
+#!/usr/bin/env Rscript
+
+CV_RMD <- here::here("cv", "cv.Rmd")
+stopifnot(file.exists(CV_RMD))
+
+# ---- CV (html) ----
+
+CV_HTML <- here::here("cv", "cv.html")
+INDEX_HTML <- here::here("docs", "index.html")
+
+rmarkdown::render(
+  CV_RMD,
+  params = list(pdf_mode = FALSE),
+  output_file = CV_HTML
+)
+
+# Copy file to "docs/index.html" for GitHub pages.
+fs::file_copy(CV_HTML, INDEX_HTML, overwrite = TRUE)
+
+# ---- CV (pdf) ----
+
+CV_PDF <- here::here("cv", "cv.pdf")
+
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render(
+  CV_RMD,
+  params = list(pdf_mode = TRUE),
+  output_file = tmp_html_cv_loc
+)
+
+# Convert to PDF using Pagedown.
+pagedown::chrome_print(input = tmp_html_cv_loc, output = CV_PDF)
